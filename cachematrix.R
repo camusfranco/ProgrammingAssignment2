9a07b8c34ae231eb89477cb8fe9f7fdf30157fe9
camusfranco <camusdfranco@gmail.com>
2020-11-04 21:59:38 -0400

    This is my first attempt to complete the perr-reviewd assigment.
    First function creates cached matrices, able to cache its inverse
    Second function solves inverse but first searching memory to see wether the inverse was already computed.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,45 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+#test
+set.seed(1234)
+cache_mat <- makeCacheMatrix(x = matrix(runif(16),4,4))
+cache_mat$getInverse()
+#NULL
+
+cacheSolve(cache_mat)
+cache_mat$getInverse()
+#Inverse was stored in cache
+
+cacheSolve(cache_mat)
+#Comment appeared
+
+
